ad559479d7ad16ec86baa831d3d57ae39e91d715
roetezadi <roetezadi@gmail.com>
2017-10-08 16:45:58 +0330

    start

---
+++ KetabKhan/KetabKhan/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KetabKhan
+{
+    class Program
+    {
+        int start = 0;
+        static void Main(string[] args)
+        {
+            //sadfs
+        }
+    }
+}
